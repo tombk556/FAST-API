0625037e159d100a84e3f0a8630d8fdda7bdf9de
Tom <tombk@web.de>
2023-05-21 11:30:27 +0200

    added users and cors api

--- app/main.py
+++ app/main.py
@@ -1,15 +1,31 @@
 from fastapi import FastAPI
 from . import models
+from fastapi.middleware.cors import CORSMiddleware
 from .database import engine
 from .config import settings
 from .routers import posts, user, auth, vote
 from pydantic import BaseSettings
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["https://www.google.com",
+           "https://www.youtube.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+@app.get("/")
+def root():
+    return {"message": "Hello World!"}
